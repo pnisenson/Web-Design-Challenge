d65392cad56611ae7ecd599ee2036773af9f529b
pnisenson <pnisenson@gmail.com>
2021-02-03 17:46:12 -0500

    changed pandas code to output cities.html to resources folder. removed testcode file as it is insignifcant. need to update readme.

--- resources/cities.py
+++ resources/cities.py
@@ -5,6 +5,6 @@ df.set_index('City_ID', inplace= True)
 
 result = df.to_html()
 
-text_file = open("../cities.html", "w") 
+text_file = open("cities.html", "w") 
 text_file.write(result) 
 text_file.close() 
